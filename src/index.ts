dedf93f8e875a71493d83c8ff84953421be1dd73
Pavel Zastavnitskiy <zastavnitskiy@Pavels-MacBook-Pro.local>
2020-04-02 16:26:22 +0200

    Add subscription functionality, handle input event, handle comma

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,21 @@ declare global {
   interface Window { EmailsInput: any }
 }
 
+interface Subscriber {
+  (values: string[]): void
+}
+
 window.EmailsInput = class EmailsInput {
 
   private values: string[]
   private node: HTMLDivElement;
   private interactiveInput: HTMLDivElement;
   private textInput: HTMLDivElement;
+  private subscribers: Subscriber[]
 
   public constructor(node: HTMLDivElement, options = {}){
     this.node = node;
-    console.log('Hello', node, options);
-    // DOM is the source of truth for the application state
-    
+    this.subscribers = [];
     const children: HTMLInputElement[] = Array.from(node.querySelectorAll('input[name="email"]'));
     children.forEach(child => child.setAttribute('type', 'hidden'));
 
@@ -28,13 +31,13 @@ window.EmailsInput = class EmailsInput {
     this.textInput.innerHTML = `<input name="text-input" type="text" value="hello" />`;
     this.node.appendChild(this.textInput);
 
-    console.log(this.node)
-
     this.render();
+    this.notifySubscribers()
 
     this.node.addEventListener('focusout', this.handleFocusOutEvent.bind(this))
     this.node.addEventListener('keydown', this.handleKeydownEvent.bind(this))
     this.node.addEventListener('click', this.handleClick.bind(this))
+    this.node.addEventListener('input', this.handleInputEvent.bind(this))
   }
 
   private processRawInput(rawInput: string): string[] {
@@ -68,11 +71,28 @@ window.EmailsInput = class EmailsInput {
     const target = event.target as HTMLInputElement;
     const keyCode = event.keyCode;
 
-    if (target.name === 'text-input' && keyCode === 13) {
-      event.preventDefault();
+    if (target.name === 'text-input') {
+      if (keyCode === 13) {
+          event.preventDefault();
+    
+          this.addValue(target.value)
+          target.value = '';
+      }
+    } 
+  }
 
-      this.addValue(target.value)
+  private handleInputEvent(event: Event) {
+    if (!event.target) {
+      return;
     }
+    
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    if (target.name === "text-input" && value.trim().endsWith(',')) {
+      this.addValue(target.value);
+      target.value = '';
+    }
+
   }
 
   private handleFocusOutEvent(event: FocusEvent) {
@@ -99,21 +119,35 @@ window.EmailsInput = class EmailsInput {
     this.interactiveInput.innerHTML = markup;
   }
 
+  private notifySubscribers() {
+    this.subscribers.forEach(subscriber => {
+      subscriber(this.values);
+    })
+  }
+
   public addValue(valueToAdd: string): void {
     const values = this.processRawInput(valueToAdd);
     values.forEach(value => this.values.push(value))
     this.render();
+    this.notifySubscribers();
   }
 
   public deleteValue(valueToDelete: string): void {
     this.values = this.values.filter(value => value !== valueToDelete);
     this.render();
+    this.notifySubscribers();
   }
   
   public getCount(): number {
     return this.values.length
   }
 
+  public subscribe(subscriber: Subscriber) {
+    this.subscribers = [...this.subscribers, subscriber];
+    subscriber(this.values);
+    return () => this.subscribers = this.subscribers.filter(existingSubscriber => existingSubscriber !== subscriber);
+  }
+ 
   
 };
 
